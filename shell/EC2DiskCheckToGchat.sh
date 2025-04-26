8721ebf26a6be8cdfc0b45cdcd43d1f2b9eb5653
mozzquito <168249163+mozzquito@users.noreply.github.com>
2025-04-26 11:14:01 +0700

    Update EC2DiskCheckToGchat.sh

--- shell/EC2DiskCheckToGchat.sh
+++ shell/EC2DiskCheckToGchat.sh
@@ -4,32 +4,20 @@
 THRESHOLD=80
 HOSTNAME=$(hostname)
 DATETIME=$(TZ='Asia/Bangkok' date '+%d-%m-%Y %H:%M:%S')
-WEBHOOK_URL="https://chat.googleapis.com/v1/spaces/AAQA101hBdE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=ynjaZw9nWJj2EJx6AWI84Rmx3svNwhD-dnsfk5NFVVw"
-PRIVATE_IP=$(hostname -I | awk '{print $1}')
-PUBLIC_IP=$(curl -s ifconfig.me)
-PUBLIC_IP_ADD=$(curl -s http://checkip.amazonaws.com)
+WEBHOOK_URL="https://chat.googleapis.com/v1/spaces/AAQAolAlBVg/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=fPohB6wPu5BlnRVpEM81Nx4MdBDXSE3DWGqYF6K7m8g"
+PUBLIC_IP=$(wget -qO- http://checkip.amazonaws.com)
+
 
 # ฟังก์ชันส่งข้อความไปยัง Google Chat
 send_gchat_message() {
   local message="$1"
-  curl -X POST -H 'Content-Type: application/json' \
+  curl -s -X POST -H 'Content-Type: application/json' \
     -d "{\"text\": \"$message\"}" \
-    "$WEBHOOK_URL"
+    "$WEBHOOK_URL" >/dev/null
 }
-# ตรวจสอบพื้นที่ดิสก์
-df -hP | grep -vE '^Filesystem|tmpfs|udev' | while read -r line; do
-  USAGE=$(echo $line | awk '{print $5}' | sed 's/%//')
-  USED=$(echo $line | awk '{print $3}')
-  TOTAL=$(echo $line | awk '{print $2}')
-  PARTITION=$(echo $line | awk '{print $6}')
 
-  if [ "$USAGE" -ge "$THRESHOLD" ]; then
-MESSAGE="*Alert Disk Storage Over 80%*
-*Time:* $DATETIME
-*Host:* $HOSTNAME
-*IPv4:* $PUBLIC_IP
-*Partition:* $PARTITION
-*Storage:* $USED / $TOTAL (${USAGE}%)"
-    send_gchat_message "$MESSAGE"
-  fi
+# ตรวจสอบพื้นที่ดิสก์
+df -P | awk 'NR>1 {gsub("%","",$5); if ($5 >= 80) print $6, $3, $2, $5}' | while read -r PARTITION USED TOTAL USAGE; do
+  MESSAGE="*Disk Alert*\nTime: $DATETIME\nHost: $HOSTNAME\nIP: $PUBLIC_IP\nPartition: $PARTITION\nStorage: $USED / $TOTAL (${USAGE}%)"
+  send_gchat_message "$MESSAGE"
 done
