9673c9adc841b16cf2aed6cf239ddb611460c143
mozzquito <168249163+mozzquito@users.noreply.github.com>
2025-04-26 13:12:23 +0700

    Update clearBufferMemory.sh

--- shell/clearBufferMemory.sh
+++ shell/clearBufferMemory.sh
@@ -3,6 +3,8 @@
 WEBHOOK_URL="https://chat.googleapis.com/v1/spaces/AAQA101hBdE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=ynjaZw9nWJj2EJx6AWI84Rmx3svNwhD-dnsfk5NFVVw"
 HOSTNAME=$(hostname)
 DATETIME=$(TZ='Asia/Bangkok' date '+%d-%m-%Y %H:%M:%S')
+PUBLIC_IP=$(wget -qO- http://checkip.amazonaws.com)
+
 
 # อ่าน Memory Info ใช้แค่ awk ไม่ใช้ grep
 MEM_INFO=$(free -h | grep ^Mem: | awk '{print "Total: "$2" | Used: "$3" | Free: "$4" | Shared: "$5" | Buff/Cache: "$6" | Available: "$7}')
@@ -28,6 +30,6 @@ echo 3 > /proc/sys/vm/drop_caches
 BUFFER_AFTER=$(get_buffer_memory)
 
 # เตรียมข้อความ
-MESSAGE="*Buffer Memory Clear*\nTime: $DATETIME\nHost: $HOSTNAME\n\n $MEM_INFO"
+MESSAGE="*Buffer Memory Clear*\nTime: $DATETIME\nHost: $HOSTNAME\nIP: $PUBLIC_IP\n\n$MEM_INFO"
 
 send_gchat_message "$MESSAGE"
