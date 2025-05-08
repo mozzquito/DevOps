47cfedeba25eac9586e59d89eeb24ea05fe2da21
mozzquito <168249163+mozzquito@users.noreply.github.com>
2025-05-08 13:54:02 +0700

    Update cpuutilization.sh
    ลบ Process อันดับ

--- shell/cpuutilization.sh
+++ shell/cpuutilization.sh
@@ -6,7 +6,6 @@ INSTANCE_NAME=$(hostname)
 TIME=$(TZ='Asia/Bangkok' date '+%d-%m-%Y %H:%M:%S')
 PRIVATE_IP=$(hostname -I | awk '{print $1}')
 PUBLIC_IP=$(wget -qO- http://checkip.amazonaws.com)
-TOP_PROCESS=$(ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6)
 
 CPU_IDLE=$(top -bn1 | grep "Cpu(s)" | awk -F'id,' -v prefix="$prefix" '{split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf("%.0f\n", v)}')
 CPU_USAGE=$((100 - CPU_IDLE))
@@ -14,6 +13,6 @@ CPU_USAGE=$((100 - CPU_IDLE))
 THRESHOLD=80
 
 if [ "$CPU_USAGE" -gt "$THRESHOLD" ]; then
-    TEXT="Alarm CPUUtilization \nTime: $TIME\nHostname: zoo-database-optimize \nIP: $PUBLIC_IP \nCPU: ${CPU_USAGE}%\n*Top CPU Processes:*\n\`\`\`\n$TOP_PROCESS\n\`\`\`"
+    TEXT="Alarm CPUUtilization \nTime: $TIME\nHostname: zoo-database-optimize \nIP: $PUBLIC_IP \nCPU: ${CPU_USAGE}%\n"
     curl -s -X POST -H 'Content-Type: application/json' -d "{\"text\": \"$TEXT\"}" "$WEBHOOK_URL" >/dev/null
 fi
